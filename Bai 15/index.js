b36d819022e2e425bc4e346db641dec6166bcdc3
anhtu289 <utc.anhtu2289@gmail.com>
2023-07-02 23:21:24 +0700

    3rd

---
+++ Bai 15/index.js
@@ -0,0 +1,15 @@
+let a = 0
+let n = 3
+for(i=0;i<=n;i++){
+
+   console.log("i", i)
+   
+}
+
+
+
+
+
+
+
+
---
+++ Bai 16/index.js
@@ -0,0 +1,18 @@
+let a = 0;
+let s = 0;
+let n = 10
+for(i=1;i<=n;i++){
+   
+    let t =  0;
+    console.log("i",(i++))
+    
+   
+    
+}
+
+
+
+
+
+
+
